5678192dd2ed7532c38d98176b83ddd20e83215f
unknown <bhavya.dang127@gmail.com>
2023-03-07 16:29:50 +0530

    hosting...

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Content-Type");
 });
 
-const PORT = process.en.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
