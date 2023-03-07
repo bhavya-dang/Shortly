f326da7a4994d03de3be639951f61082b05c883c
unknown <bhavya.dang127@gmail.com>
2023-03-07 16:14:19 +0530

    made changes for to host api on render

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // import express from 'express';
 // import connectDB from './config/db.js';
 // import cors from "cors";
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 const cors = require("cors");
 const app = express();
 
@@ -12,16 +12,18 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-
-// Define Routes
-app.use('/', require('./routes/index'));
-app.use('/api/v1/url', require('./routes/url'));
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+app.get("/status", (req, res) => {
+  res.send("Server works.");
 });
 
+// Define Routes
+app.use("/", require("./routes/index"));
+app.use("/api/v1/url", require("./routes/url"));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+});
 
-const PORT = 3000;
+const PORT = process.en.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
